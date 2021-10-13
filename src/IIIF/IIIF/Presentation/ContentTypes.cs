dca7d2bdfdde1240806250600deb9811f8f24788
Donald Gray <donald.gray@digirati.com>
2021-10-13 17:34:57 +0100

    Add contentTypes

---
+++ src/IIIF/IIIF/Presentation/ContentTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace IIIF.Presentation
+{
+    /// <summary>
+    /// Contains Content-Type/Accepts headers for IIIF Presentation API. 
+    /// </summary>
+    public static class ContentTypes
+    {
+        /// <summary>
+        /// Content-Type for IIIF presentation 2.
+        /// </summary>
+        public const string V2 = "application/ld+json;profile=\"" + Context.Presentation2Context + "\"";
+
+        /// <summary>
+        /// Content-Type for IIIF presentation 3. 
+        /// </summary>
+        public const string V3 = "application/ld+json;profile=\"" + Context.Presentation3Context + "\"";
+    }
+}
