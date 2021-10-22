7d2052ace30cddc1e9515a297d5f86edbbf8ffb5
Magnus Welander <magnus.welander@riksarkivet.se>
2021-10-22 13:17:27 +0200

    Removed the conditionals regarding a targeted Canvas. It was necessary for outputting the Annotation.Target property which is a canvas which in our case is an ancestor that holds a lot of information.

--- src/IIIF/IIIF/Serialisation/TargetConverter.cs
+++ src/IIIF/IIIF/Serialisation/TargetConverter.cs
@@ -40,14 +40,10 @@ namespace IIIF.Serialisation
         {
             if (value is Canvas canvas)
             {
-                if (canvas.Width == null && canvas.Duration == null && (canvas.Items == null || !canvas.Items.Any()))
-                {
-                    writer.WriteValue(canvas.Id);
-                    return;
-                }
+                writer.WriteValue(canvas.Id);
+                return;
             }
-            // Default, pass through behaviour:
-            JObject.FromObject(value, serializer).WriteTo(writer);
+            // TODO: add other types
         }
     }
 }
