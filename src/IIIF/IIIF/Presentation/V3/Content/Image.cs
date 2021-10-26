086b2e1719e4bf8f47a31c534c15750e76934f00
Magnus Welander <magnus.welander@riksarkivet.se>
2021-10-26 09:36:13 +0200

    Allowing null values for width and height since they are not mandatory For example the logo property does not need dimensions

--- src/IIIF/IIIF/Presentation/V3/Content/Image.cs
+++ src/IIIF/IIIF/Presentation/V3/Content/Image.cs
@@ -6,10 +6,10 @@ namespace IIIF.Presentation.V3.Content
     public class Image : ExternalResource, ISpatial, IPaintable
     {
         [JsonProperty(Order = 11)]
-        public int Width { get; set; }
+        public int? Width { get; set; }
         
         [JsonProperty(Order = 12)]
-        public int Height { get; set; }
+        public int? Height { get; set; }
 
         public Image() : base(nameof(Image)) {}
     }
--- src/IIIF/IIIF/Presentation/V3/Content/Video.cs
+++ src/IIIF/IIIF/Presentation/V3/Content/Video.cs
@@ -4,8 +4,8 @@ namespace IIIF.Presentation.V3.Content
 {
     public class Video : ExternalResource, ISpatial, ITemporal, IPaintable
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
         public double Duration { get; set; }
 
         public Video() : base(nameof(Video)) { }
