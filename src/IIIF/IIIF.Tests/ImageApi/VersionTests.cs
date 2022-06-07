5cc7bbe884e2543e7238deaab84e8c4650980a8e
Donald Gray <donald.gray@digirati.com>
2022-06-07 12:36:01 +0100

    Add ImageVersion enum

---
+++ src/IIIF/IIIF.Tests/ImageApi/VersionTests.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using FluentAssertions;
+using IIIF.ImageApi;
+using Xunit;
+
+namespace IIIF.Tests.ImageApi
+{
+    public class VersionTests
+    {
+        [Theory]
+        [InlineData(Version.Unknown, "Unknown")]
+        [InlineData(Version.V2, "http://iiif.io/api/image/2/context.json")]
+        [InlineData(Version.V3, "http://iiif.io/api/image/3/context.json")]
+        public void VersionsHaveCorrectDescriptions(Version version, string context)
+        {
+            var fieldInfo = typeof(Version).GetField(version.ToString());
+            var actual = fieldInfo!
+                .GetCustomAttribute<DisplayAttribute>()!
+                .Description;
+
+            actual.Should().Be(context);
+        }
+    }
+}
---
+++ src/IIIF/IIIF/ImageApi/Version.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IIIF.ImageApi
+{
+    /// <summary>
+    /// Available IIIF Image API Versions.
+    /// </summary>
+    public enum Version
+    {
+        /// <summary>
+        /// Fallback value, unknown version.
+        /// </summary>
+        [Display(Description = "Unknown")]
+        Unknown = 0,
+        
+        /// <summary>
+        /// IIIF Image API version 2.
+        /// </summary>
+        [Display(Description = Service.ImageService2.Image2Context)]
+        V2,
+        
+        /// <summary>
+        /// IIIF Image API version 3.
+        /// </summary>
+        [Display(Description = Service.ImageService3.Image3Context)]
+        V3
+    }
+}
