d60e3e477580469768f4066823bad51cce22bc13
Donald Gray <donald.gray@digirati.com>
2022-06-07 17:20:14 +0100

    Add imageApi content-types

---
+++ src/IIIF/IIIF/ImageApi/ContentTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace IIIF.ImageApi
+{
+    /// <summary>
+    /// Contains Content-Type/Accepts headers for IIIF Image API. 
+    /// </summary>
+    public static class ContentTypes
+    {
+        /// <summary>
+        /// Content-Type for IIIF Image 2.
+        /// </summary>
+        public const string V2 = "application/ld+json;profile=\"" + Service.ImageService2.Image2Context + "\"";
+
+        /// <summary>
+        /// Content-Type for IIIF Image 3. 
+        /// </summary>
+        public const string V3 = "application/ld+json;profile=\"" + Service.ImageService3.Image3Context + "\"";
+    }
+}
