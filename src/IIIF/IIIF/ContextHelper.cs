b197a114c05c8add557e6bf3dfd7681f7af4b5a5
Donald Gray <donald.gray@digirati.com>
2023-08-09 17:09:43 +0100

    Move EnsureContext method to own class + add imageApi to known list

---
+++ src/IIIF/IIIF/ContextHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IIIF;
+
+public static class ContextHelper
+{
+    // This is a list of known IIIF contexts that must be last in Contexts list
+    // Only 1 can be present in any given Context list
+    private static List<string> knownContexts = new()
+    {
+        Presentation.Context.Presentation2Context,
+        Presentation.Context.Presentation3Context,
+        ImageApi.V2.ImageService2.Image2Context,
+        ImageApi.V3.ImageService3.Image3Context,
+    };
+    
+    /// <summary>
+    /// Adds specified Context to list.
+    /// The IIIF context must be last in the list, to override any that come before it.
+    /// </summary>
+    public static void EnsureContext(this JsonLdBase resource, string contextToEnsure)
+    {
+        if (resource.Context == null)
+        {
+            resource.Context = contextToEnsure;
+            return;
+        }
+        
+        var workingContexts = GetWorkingContexts(resource);
+        workingContexts.Add(contextToEnsure);
+
+        if (workingContexts.Intersect(knownContexts).Count() > 1)
+        {
+            throw new InvalidOperationException(
+                "You cannot have multiple IIIF contexts (Presentation or Image) in the same resource.");
+        }
+
+        var iiifContext = workingContexts.FirstOrDefault(wc => knownContexts.Contains(wc));
+
+        // If we have an IIIF context it must be last in list
+        if (!string.IsNullOrEmpty(iiifContext))
+        {
+            workingContexts.Remove(iiifContext);
+            workingContexts.Add(iiifContext);
+        }
+
+        // Now JSON-LD rules. The @context is the only Presentation 3 element that has this.
+        resource.Context = workingContexts.Count == 1 ? workingContexts[0] : workingContexts;
+    }
+
+    private static List<string> GetWorkingContexts(JsonLdBase resource)
+    {
+        if (resource.Context is List<string> existingContexts) return existingContexts;
+        
+        if (resource.Context is string singleContext) return new List<string> { singleContext };
+
+        return new List<string>(1);
+    }
+}
--- src/IIIF/IIIF/Presentation/Context.cs
+++ src/IIIF/IIIF/Presentation/Context.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace IIIF.Presentation;
+﻿namespace IIIF.Presentation;
 
 /// <summary>
 /// Contains JSON-LD Contexts for IIIF Presentation API.
@@ -26,56 +23,5 @@ public static class Context
     public static void EnsurePresentation2Context(this JsonLdBase resource)
     {
         resource.EnsureContext(Presentation2Context);
-    }
-
-    // The IIIF context must be last in the list, to override any that come before it.
-    public static void EnsureContext(this JsonLdBase resource, string contextToEnsure)
-    {
-        if (resource.Context == null)
-        {
-            resource.Context = contextToEnsure;
-            return;
-        }
-
-        List<string> workingContexts = new();
-        if (resource.Context is List<string> existingContexts) workingContexts = existingContexts;
-
-        if (resource.Context is string singleContext) workingContexts = new List<string> { singleContext };
-
-        List<string> newContexts = new();
-        var requiresPresentation3Context = contextToEnsure == Presentation3Context;
-        var requiresPresentation2Context = contextToEnsure == Presentation2Context;
-        foreach (var workingContext in workingContexts)
-            switch (workingContext)
-            {
-                case Presentation3Context:
-                    requiresPresentation3Context = true;
-                    break;
-                case Presentation2Context:
-                    requiresPresentation2Context = true;
-                    break;
-                default:
-                    newContexts.Add(workingContext);
-                    break;
-            }
-
-        // Add the new context to the list but not if it supposed to come last
-        if (!newContexts.Contains(contextToEnsure)
-            && contextToEnsure != Presentation3Context
-            && contextToEnsure != Presentation2Context)
-            newContexts.Add(contextToEnsure);
-
-        if (requiresPresentation2Context && requiresPresentation3Context)
-            throw new InvalidOperationException(
-                "You cannot have Presentation 2 and Presentation 3 contexts in the same resource.");
-        // These have to come last
-        if (requiresPresentation3Context) newContexts.Add(Presentation3Context);
-        if (requiresPresentation2Context) newContexts.Add(Presentation2Context);
-
-        // Now JSON-LD rules. The @context is the only Presentation 3 element that has this.
-        if (newContexts.Count == 1)
-            resource.Context = newContexts[0];
-        else
-            resource.Context = newContexts;
     }
 }
