659445942fa5e8ccf610e8d374bb20df10dbea9a
Donald Gray <donald.gray@digirati.com>
2024-10-28 11:22:28 +0000

    Use theory for cookbook tests
    This allows easier visibility of which tests pass and fail

--- src/IIIF/IIIF.Tests/Serialisation/CookbookDeserialization.cs
+++ src/IIIF/IIIF.Tests/Serialisation/CookbookDeserialization.cs
@@ -1,49 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
 using IIIF.Presentation.V3;
-using IIIF.Serialisation;
+using IIIF.Tests.Serialisation.Data;
 
 namespace IIIF.Tests.Serialisation;
 
+[Trait("Category", "Cookbook")]
 public class CookbookDeserialization
 {
-    private Collection theseusCollection;
-    private HttpClient httpClient;
-    private List<string> skip;
-    
-    public CookbookDeserialization()
+    [Theory]
+    [ClassData(typeof(CookbookManifestData))]
+    public void Can_Deserialize_Cookbook_Manifest(string manifestId, Manifest manifest)
     {
-        httpClient = new HttpClient();
-        var s = httpClient.GetStringAsync("https://theseus-viewer.netlify.app/cookbook-collection.json").Result;
-        theseusCollection = s.FromJson<Collection>();
-        
-        // these have bugs in the cookbook, see https://github.com/IIIF/cookbook-recipes/pull/546
-        skip = new List<string>
-        {
-            "https://iiif.io/api/cookbook/recipe/0219-using-caption-file/manifest.json",
-            "https://iiif.io/api/cookbook/recipe/0040-image-rotation-service/manifest-service.json"
-        };
+        // perfunctory assertion
+        manifest.Should().NotBeNull();
+        manifest.Id.Should().Be(manifestId);
     }
-
-    [Fact]
-    public void Can_Deserialize_Cookbook_Collection()
-    {
-        foreach (var item in theseusCollection.Items!)
-        {
-            if (item is Manifest manifestRef)
-            {
-                if (!skip.Contains(manifestRef.Id))
-                {
-                    var s = httpClient.GetStringAsync(manifestRef.Id).Result;
-                    var manifest = s.FromJson<Manifest>();
-                    // perfunctory assertion
-                    manifest.Id.Should().Be(manifestRef.Id);
-                }
-            }
-            // Do collections too...
-        }
-        
-    }
-    
 }
